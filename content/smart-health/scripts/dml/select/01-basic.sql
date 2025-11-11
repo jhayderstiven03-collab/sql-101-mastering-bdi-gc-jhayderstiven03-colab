4055342b1c089502e29b4684133584bced365112
jhayderstiven03-collab <jhayderstiven03@gmail.com>
2025-11-11 15:12:57 -0500

    feat(data): add smart_health nomalization file

--- content/smart-health/scripts/dml/select/01-basic.sql
+++ content/smart-health/scripts/dml/select/01-basic.sql
@@ -27,15 +27,46 @@ LIMIT 10;
 -- 2️⃣ La segunda consulta selecciona los nombres completos de los médicos activos junto con su número de licencia médica, 
 --ordenando alfabéticamente por apellidos. También aplica alias a las columnas para mostrar un encabezado más legible y 
 --limita el resultado a los primeros 20 doctores.
+SELECT 
+    'Dr. '||first_name||' '||last_name as médico,
+    medical_license_number as licencia_médica
+FROM doctors 
+WHERE  active = TRUE
+ORDER BY last_name DESC
+LIMIT 20;
  
 -- 3️⃣ La tercera consulta obtiene el listado de municipios y sus respectivos departamentos, 
 --niendo ambas tablas mediante la clave foránea department_code. Se ordena por el nombre del departamento 
 --para facilitar la localización geográfica, mostrando los 15 primeros resultados.
- 
+
+SELECT 
+    T1.municipality_name AS municipio, 
+    T2.department_name AS departamento
+FROM municipalities T1
+JOIN departments T2 ON T1.department_code = T2.department_code
+ORDER BY departamento
+LIMIT 15;
+
 -- 4️⃣ La cuarta consulta selecciona las citas médicas programadas (tabla appointment), 
 --mostrando el tipo de cita, el estado actual y la fecha correspondiente. Se utiliza un alias 
 --para cada campo y se ordena por fecha de cita en orden ascendente, limitando la salida a las próximas 10 citas.
+
+SELECT 
+    T1.appointment_type AS TIPO,
+    T1.status AS estado_actual,
+    T1.appointment_date AS fecha
+
+FROM appointments T1
+ORDER BY T1.appointment_date ASC 
+LIMIT 10
  
 -- 5️⃣ Finalmente, la quinta consulta obtiene los nombres comerciales de los medicamentos junto con su 
 --ingrediente activo, presentándolos de forma alfabética. Se usa alias para mejorar la presentación de los
 --encabezados y un límite de 25 registros, ideal para una vista rápida del catálogo farmacológico disponible.
+
+SELECT
+    T1.commercial_name AS nombre_comercial,
+    T1.active_ingredient AS ingrediente_activo
+FROM medications T1
+ORDER BY T1.commercial_name 
+LIMIT 25
